a99b5b7636ad01c022e8e3a80219a043099a0106
TRI VI <trivi2710@gmail.com>
2017-06-27 11:22:46 -0400

    first init

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var socket = require('socket.io');
+
+//set up app
+var app = express();
+
+//set up server
+var server = app.listen(4000, function(){
+	console.log("Listening on port 4000");
+});
+
+// set up static public folder
+app.use(express.static('public'));
+
+//set up socket for server
+var io = socket(server);
