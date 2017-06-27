fda30ec1e9c8c498e1b560a44dbe305fff24a8bf
TRI VI <trivi2710@gmail.com>
2017-06-27 18:58:53 -0400

    adding typing event to server
    Feat: adding typing event to the server.
    
    Typing event will be fired when the user emit keypress function. All
    the other clients except the current typing client will see who is
    typing.

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ io.on('connection', function (socket){
 		console.log(data);
 		io.sockets.emit('chat', data);
 	});
+
+	socket.on('typing', function(data){
+		socket.broadcast.emit('typing', data)
+	})
 });
 
 
